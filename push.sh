4adb2edbe55298fc3c0132c0077955ee3b5757ee
Your Name <you@example.com>
2025-09-12 19:23:29 +0700

    update

--- push.sh
+++ push.sh
@@ -1,4 +1,6 @@
+# git remote set-url origin git@github:itgiup/fullstack-web3-dapp
+
 git add .
 git commit -m "update"
 git push
-git remote set-url origin git@github-itgiup:itgiup/fullstack-web3-dapp
+
